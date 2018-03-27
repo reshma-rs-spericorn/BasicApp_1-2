dd691bcad26e52d9b0ae764e0df57b5a2cc7aa8c
reshma-rs-spericorn <reshmars@spericorn>
2018-03-27 18:16:29 +0530

    nested route using match url

--- src/components/NestedRoute.js
+++ src/components/NestedRoute.js
@@ -1,87 +1,56 @@
 import React from "react";
-
+import Home from './Home';
+import Topics from './Topics';
+import About from './About';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const Homee = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
 
-const About = ({ routes }) => (
-  <div>
-    <h2>About</h2>
-    <ul>
-      {routes.map(({ path, name }) => (
-        <li><Link to={path}>{name}</Link></li>
-      ))}
-    </ul>
-    {routes.map((route) => (
-      <RouteWithSubRoutes key={route.path} {...route} />
-    ))}
-  </div>
-);
-const Bus = () => (
-  <div>
-    <h2>Bus</h2>
-  </div>
-);
 
-const Cart = () => (
-  <div>
-    <h2>Cart</h2>
-  </div>
-);
+
 
 //routes
 const routes = [
   {
     path: '/home',
-    component: Homee,
+    component: Home,
   },
   {
     path: '/about',
     component: About,
-    routes: [
-      {
-        name: 'Bus',
-        path: '/about/bus',
-        component: Bus,
-      },
-      {
-        name: 'Cart',
-        path: '/about/cart',
-        component: Cart,
-      }
-    ]
+  },
+  {
+    path:'/topics',
+    component: Topics,
   }
 ]
 
-const RouteWithSubRoutes = (route) => (
-  <Route path={route.path} render={(props) => (
-    <route.component {...props} routes={route.routes} />
-  )} />
-)
 
 class App extends React.Component {
 
   render() {
     return (
       <Router>
-        <div>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-        {routes.map((route) => (
-          <RouteWithSubRoutes key={route.path} {...route} />
+    <div>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+      {routes.map((route) => (
+         <Route
+         key={route.path}
+         path={route.path}
+         component={route.component}/>
         ))}
-</div>
-      </Router>
+      <hr />
+    </div>
+  </Router>
     )
   }
 }
